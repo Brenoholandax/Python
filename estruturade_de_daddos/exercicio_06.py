6e87376fe4b9819905594920063f24b964999d2e
Breno Holanda <120995868+Brenoholandax@users.noreply.github.com>
2024-05-20 14:37:43 -0300

    Correções

--- estruturade_de_daddos/exercicio_06.py
+++ estruturade_de_daddos/exercicio_06.py
@@ -1,19 +1,19 @@
 def prever_fruta(peso, textura, cor):
     # Lógica de decisão baseada nas características fornecidas
-    if peso >= 150 and textura == "smooth" and cor == "red":
+    if peso >= 150 and textura == "aspera" and cor == "vermelha":
         return "A fruta é um morango!"
-    elif peso >= 130 and textura == "rough" and cor == "red":
+    elif peso >= 130 and textura == "suave" and cor == "vermelha":
         return "A fruta é uma maçã!"
-    elif peso >= 120 and textura == "smooth" and cor == "orange":
+    elif peso >= 120 and textura == "suave" and cor == "laranja":
         return "A fruta é uma laranja!"
-    elif peso >= 150 and textura == "rough" and cor == "yellow":
+    elif peso >= 150 and textura == "aspera" and cor == "amarela":
         return "A fruta é uma banana!"
     else:
         return "Não foi possível classificar a fruta."
 
 # Entrada do usuário
 peso_fruta = float(input("Peso da fruta (em gramas): "))
-textura_fruta = input("Textura da fruta (suave ou áspera): ")
+textura_fruta = input("Textura da fruta (suave ou aspera): ")
 cor_fruta = input("Cor da fruta (vermelha, laranja ou amarela): ")
 
 # Chamada da função de previsão e saída da previsão
