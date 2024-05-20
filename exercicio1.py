aaf7fec665e1b5b228719581e78df6bee54753ac
Breno Holanda <120995868+Brenoholandax@users.noreply.github.com>
2024-05-20 11:47:15 -0300

    novos estudos

---
+++ exercicio1.py
@@ -0,0 +1,33 @@
+saldo = 21
+print("Olá eu sou o robô do Jornal do Commercio")
+idade = float(input("Informe por favor sua idade : "))
+
+if saldo <= idade : 
+    print("Você é Gay")
+    
+    if saldo == idade :
+        print("Você é muito gay")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
---
+++ exercicio_1.py
@@ -0,0 +1,11 @@
+# Desafio: A Aventura do Explorador
+
+# Entrada
+quantidade_passos = int(input())
+
+#: Adicione uma condição para verificar se a quantidade de passos é positiva.
+# Se a quantidade de passos for zero, imprima "Nenhum passo dado na floresta."
+# Caso contrário, utilize um loop for para imprimir a mensagem do explorador, indicando o número do passo.
+#for passos in quantidade_passos :
+print(quantidade_passos)
+        
