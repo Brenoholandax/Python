b3d1e0397af379546540962078882c01e1d19ad3
Breno Holanda <120995868+Brenoholandax@users.noreply.github.com>
2024-05-24 14:15:49 -0300

    24/05/2024

---
+++ exercicios/exercicio_2.py
@@ -0,0 +1,10 @@
+
+contador = 0
+Atribuir = 0
+while contador < 101 :
+    print(f'contaodor agora está no {contador}')
+    contador +=1
+    if contador == 9 :
+      Atribuir +=1
+      break
+    print(Atribuir)
